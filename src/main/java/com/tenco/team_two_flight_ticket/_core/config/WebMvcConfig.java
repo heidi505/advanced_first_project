c0128433daa746e0f879b6c1080bb9ac9ff3a673
heidi505 <xxwhite19@gmail.com>
2023-12-01 21:33:58 +0900

    인터셉터 완료 - 등록 미완료

---
+++ src/main/java/com/tenco/team_two_flight_ticket/_core/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package com.tenco.team_two_flight_ticket._core.config;
+
+import com.tenco.team_two_flight_ticket.handler.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+}
