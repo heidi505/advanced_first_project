3e2835fe7a7e1b1185fdbd8208a5ce0da36012e1
heidi505 <xxwhite19@gmail.com>
2023-12-01 21:42:35 +0900

    인터셉터 완료 - 등록 완료

--- src/main/java/com/tenco/team_two_flight_ticket/_core/config/WebMvcConfig.java
+++ src/main/java/com/tenco/team_two_flight_ticket/_core/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.tenco.team_two_flight_ticket._core.config;
 import com.tenco.team_two_flight_ticket.handler.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,4 +11,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private AuthInterceptor authInterceptor;
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //기능 추가되면 pathpattern 추가하기
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/auth/**");
+    }
 }
