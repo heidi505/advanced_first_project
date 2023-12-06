db650ba93a2694e51d7d110ffc9eba8a81bbd488
heidi505 <xxwhite19@gmail.com>
2023-12-06 15:09:36 +0900

    쟈근 변경

--- src/main/java/com/tenco/team_two_flight_ticket/user/UserService.java
+++ src/main/java/com/tenco/team_two_flight_ticket/user/UserService.java
@@ -125,7 +125,7 @@ public class UserService {
             helper.setFrom("xxwhite19@gmail.com");
             helper.setTo(email);
             helper.setSubject("님부스의 인증 메일입니다");
-            helper.setText("나의 APP을 방문해주셔서 감사합니다." + 	//html 형식으로 작성 !
+            helper.setText("님부스를 방문해주셔서 감사합니다." + 	//html 형식으로 작성 !
                     "<br><br>" +
                     "인증 번호는 " + authNumber + "입니다." +
                     "<br>" +
