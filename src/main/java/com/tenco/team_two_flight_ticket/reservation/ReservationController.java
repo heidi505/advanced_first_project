629aa7e51913db31ec03ec8c4939e1e6a7f8c4bb
MinkyeongSung~ <wildlegion59@gmail.com>
2023-12-06 20:46:41 +0900

    운임규정 모달

--- src/main/java/com/tenco/team_two_flight_ticket/reservation/ReservationController.java
+++ src/main/java/com/tenco/team_two_flight_ticket/reservation/ReservationController.java
@@ -26,4 +26,11 @@ public class ReservationController {
     public String test4(){
         return"reservation/detail";
     }
+
+    // 운임규정 모달 버튼 (위치 잡고 추가만 해주면 됨!)
+    // 예약규정, 운임규정, 결제규정, 환불/변경
+    @GetMapping("/regulation")
+    public String test05(){
+        return "reservation/fareRegulation";
+    }
 }
