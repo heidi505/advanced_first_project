528067363aa1916d5620c271400ae0ba32705f54
seunggod <kiss0703@nate.com>
2023-12-08 15:43:32 +0900

    complete

--- src/main/java/com/tenco/team_two_flight_ticket/airport/AirportController.java
+++ src/main/java/com/tenco/team_two_flight_ticket/airport/AirportController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import com.tenco.team_two_flight_ticket._core.utils.Define;
+
 @RequestMapping("/airport")
 @Controller
 public class AirportController {
@@ -24,26 +26,27 @@ public class AirportController {
 	@GetMapping("/airport-info")
 	public String parkingArea(Model model) {
 		
+		// 공항을 인자로 받아야 함 
+		String airport = "GMP";
+		
 		URI uri = null;
-		String url = "http://openapi.airport.co.kr/service/rest/AirportParkingFee/parkingfee?serviceKey=rrf%2Bmnq9ofBCLMm6ehZUvWu%2FZljoJtXJZKSVOIkz61hIbsnmpY3s3aeMuC3VfTlt9MVM8aSL1J3M%2Bzm3ad2%2BXg%3D%3D&schAirportCode=GMP&type=json";
+		String url = "http://openapi.airport.co.kr/service/rest/AirportParkingFee/parkingfee?serviceKey="
+				+ Define.SERVICEKEY
+				+ "&schAirportCode=" + airport
+				+ "&type=json";
 		try {
 			uri = new URI(url);
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		} 
 				 
-		
-		System.out.println(uri.toString());
 		RestTemplate restTemplate = new RestTemplate();
-		
 		HttpHeaders headers = new HttpHeaders();
- 
 		HttpEntity<MultiValueMap<String, String>> request 
 				= new HttpEntity<>(headers);
 		
 		ResponseEntity<Map> response = restTemplate.exchange(uri, HttpMethod.GET, request, Map.class);
 		model.addAttribute("list",response.getBody());
-		System.out.println(response.getBody());
 		
 		
 		return "airport/airportInfo";
