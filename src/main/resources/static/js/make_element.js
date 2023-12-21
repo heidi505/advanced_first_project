51606474f560609a5c31b07fc9dff50baf0e5c40
seunggod <kiss0703@nate.com>
2023-12-21 21:42:31 +0900

    dummy

---
+++ src/main/resources/static/js/make_element.js
@@ -0,0 +1,12 @@
+// 요소 타입과 클래스명을 받아 생성하는 함수
+function makeElement(elementType, classString){
+	let element = document.createElement(elementType);
+	// 클래스명이 없다면 생략
+	if(classString != undefined){
+		classArray = classString.split(' ');
+		classArray.forEach(name=>{
+			element.classList.add(name);
+		})
+	}
+	return element;
+};
